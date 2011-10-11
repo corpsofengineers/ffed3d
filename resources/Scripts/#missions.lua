613a52ba2d68f5de1ea5262f48eb9cc6047712d8
tehnomag69 <tehnomag69@gmail.com>
2011-10-11 10:47:37 +0000

    Lua minor changes

--- resources/Scripts/#missions.lua
+++ resources/Scripts/#missions.lua
@@ -1,20 +1,21 @@
-function OnSystemInit()
+function OnSystemInit2()
 
-	SetLastAttackedIndex(0);
+	SetLastAttakedIndex(0);
 	CreateShips();
 
-	for i=1, 0x72, 1 do
+	for index=1, 0x72, 1 do
 
-		if CheckDATAObjectArray(i, 0x4b) == false then i=i+1; end
-		if i == GetPlayerIndex() then i=i+1; end
+		if GetObjectState(i) ~= IST_DOCKED and i ~= GetPlayerIndex() then
 
-		obj = GetObject(i);
+			obj = GetObject(i);
 
-		if obj.ai_mode == AI_DOCKED_OR_LANDED then
+			if obj.ai_mode == AI_DOCKED_OR_LANDED then
 
-			starport = GetObject (obj.dest_index);
+				starport = GetObject(obj.dest_index);
 
-			if IsStarportLocked(starport) == true then DestroyObject(obj, 0); end
+				if IsStarportLocked(startport) == true then DestroyObject(obj, 0) end
+
+			end
 
 		end
 
